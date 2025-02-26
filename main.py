a012cdc3bd87e0830fdf96a28fe2de8e86f506ac
Satvik Gonur <sgonur@ucsc.edu>
2025-02-25 16:54:55 -0800

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import matplotlib.pyplot as plt
+from PIL import Image
+import rand
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from sklearn.preprocessing import StandardScaler
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
